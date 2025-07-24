b609aa4c4caea727431f59fcbbd47db7696d8b7b
신준규 <tlswnsrb1357@gmail.com>
2025-07-24 16:06:45 +0900

    strings 추가

---
+++ strings.go
@@ -0,0 +1,15 @@
+package goutils
+
+import "strings"
+
+func splitAndTrim(s string) []string {
+	parts := strings.Split(s, ",")
+	var result []string
+	for _, part := range parts {
+		trimmed := strings.TrimSpace(part)
+		if trimmed != "" {
+			result = append(result, trimmed)
+		}
+	}
+	return result
+}
