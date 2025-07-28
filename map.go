cb0bc4bcf9a2c095e2b6cd42b8f8107d8dfc0c9a
신준규 <tlswnsrb1357@gmail.com>
2025-07-28 13:37:53 +0900

    map + 주석 추가

---
+++ map.go
@@ -0,0 +1,10 @@
+package goutils
+
+// 문자열을 키로 가지는 맵을 입력 받아 그 값을 키로, 키를 값으로 가지는 맵을 반환합니다.
+func ReverseMapp[T comparable](m map[string]T) map[T]string {
+	res := make(map[T]string)
+	for k, v := range m {
+		res[v] = k
+	}
+	return res
+}
--- strings.go
+++ strings.go
@@ -2,6 +2,7 @@ package goutils
 
 import "strings"
 
+// SplitAndTrim는 문자열을 쉼표로 분리하고 공백을 제거하여 슬라이스로 반환합니다.
 func SplitAndTrim(s string) []string {
 	parts := strings.Split(s, ",")
 	var result []string
